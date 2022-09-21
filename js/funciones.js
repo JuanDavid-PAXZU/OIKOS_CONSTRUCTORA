23cf49312f3d7775628c6e2d679d5f1362ab02d3
Juan David <juan.reyes@paxzu.co>
2022-09-21 16:52:52 -0500

    estilos basicos

---
+++ js/funciones.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+    /**** desplegar menu principal en movil ****/
+
+    $('.btn_menu_movil').click(function(){
+        
+        if($(this).hasClass('active')){
+            $(this).removeClass('active');
+            $('.nav').removeClass('active');
+        }else{
+            $(this).addClass('active');
+            $('.nav').addClass('active');
+        }
+    });
+
+    /*********
+    Aplica smaller al header con sccroll
+    **********/
+    $(function () {
+        $('a[href*=\\#]').click(function () {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname) {
+                var $target = $(this.hash);
+                $target = $target.length && $target || $('[name=' + this.hash.slice(1) + ']');
+                if ($target.length) {
+                    var targetOffset = $target.offset().top;
+                    $('html,body').animate({
+                        scrollTop: targetOffset
+                    }, 1000);
+                    return false;
+                }
+            }
+        });
+    });
+
+    /* FUNCION ARROWS FOOTER */
+
+    function navFooter(){
+    
+        let btn = document.querySelectorAll('.col_mid_men_pie .h_cl_pie');
+        let container = document.querySelectorAll('.menu_pie');
+        
+        $(btn).click( function(){
+
+            if( $(this).hasClass('active') ){
+                $(this).next(container).slideUp();
+                $(this).removeClass('active');
+            }else{
+                $(container).slideUp();
+                $(btn).removeClass('active');
+                $(this).addClass('active');
+                $(this).next().slideToggle();
+            }
+
+        });
+
+    }
+
+    if ($(window).width() < 768) {
+        navFooter();
+    }
+
+})
